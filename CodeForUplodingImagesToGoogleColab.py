74ad0152a7caa54bf3f34d6c37b40ed3ba1f2afc
pbissah <92878201+pbissah@users.noreply.github.com>
2022-03-28 23:16:36 -0400

    Update CodeForUplodingImagesToGoogleColab.py
    A test code to shuffle the images and split them in half

--- CodeForUplodingImagesToGoogleColab.py
+++ CodeForUplodingImagesToGoogleColab.py
@@ -1,9 +1,15 @@
 Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
 !gdown https://drive.google.com/uc?id=1xgk7svdjBiEyzyUVoZrCz4PP6dSjVL8S
+  ##upload the PASCAL files
 def upload_files():
   from google.colab import files
   uploaded = files.upload()
   for k, v in uploaded.items():
     open(k, 'wb').write(v)
   return list(uploaded.keys())
+##shuffling the images randomly then selecting half for training set and data set
+    random.shuffle(data)
+    train_data = data[:50]
+    test_data = data[50:]
+
