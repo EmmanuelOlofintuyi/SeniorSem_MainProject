f464c17fca1bd85b4c037786bdfc2201ee2e445d
pbissah <92878201+pbissah@users.noreply.github.com>
2022-03-28 22:56:58 -0400

    Update CodeForUplodingImagesToGoogleColab.py
    allows the uploaded images to be saved

--- CodeForUplodingImagesToGoogleColab.py
+++ CodeForUplodingImagesToGoogleColab.py
@@ -1,6 +1,9 @@
 Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
 !gdown https://drive.google.com/uc?id=1xgk7svdjBiEyzyUVoZrCz4PP6dSjVL8S
-from google.colab import files
-files.upload()
-
+def upload_files():
+  from google.colab import files
+  uploaded = files.upload()
+  for k, v in uploaded.items():
+    open(k, 'wb').write(v)
+  return list(uploaded.keys())
