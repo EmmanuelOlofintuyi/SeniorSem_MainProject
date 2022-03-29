4775b866d42e54626b8ac0104701d0065cd72cbb
pbissah <92878201+pbissah@users.noreply.github.com>
2022-03-28 23:50:12 -0400

    Update CodeForUplodingImagesToGoogleColab.py

--- CodeForUplodingImagesToGoogleColab.py
+++ CodeForUplodingImagesToGoogleColab.py
@@ -1,6 +1,9 @@
 Python 3.10.4 (v3.10.4:9d38120e33, Mar 23 2022, 17:29:05) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
 !gdown https://drive.google.com/uc?id=1xgk7svdjBiEyzyUVoZrCz4PP6dSjVL8S
+  
+  import sklearn.model_selection as model_selection
+  
   ##upload the PASCAL files
 def upload_files():
   from google.colab import files
@@ -9,7 +12,9 @@ def upload_files():
     open(k, 'wb').write(v)
   return list(uploaded.keys())
 ##shuffling the images randomly then selecting half for training set and data set
-    random.shuffle(data)
-    train_data = data[:50]
-    test_data = data[50:]
+from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(DATA_IMAGES,
+                                                    DATA_LABELS,
+                                                    test_size = 0.15,
+                                                    random_state = 41)
 
