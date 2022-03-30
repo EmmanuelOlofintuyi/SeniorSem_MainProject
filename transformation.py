e69ff6d3f0f9913b818375093b4653dba2990ca9
acherestal <70119956+acherestal@users.noreply.github.com>
2022-03-29 22:28:55 -0400

    Update transformation.py

--- transformation.py
+++ transformation.py
@@ -7,4 +7,4 @@ import sklearn.model_selection as model_selection
 
 ## transformations
 transform = transforms.Compose([transforms.ToTensor(),transforms.CenterCrop((200,100)), 
-                                transforms.Normalize((0.4363,0.4328,0.3291),(0.2132,0.2078,0.2040)])
+                                transforms.Normalize((mean_and_std[0]),(mean_and_std[1])])
