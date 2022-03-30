4d11ed89bed238fc8d2ff61bd2931633a702eb2f
acherestal <70119956+acherestal@users.noreply.github.com>
2022-03-29 22:50:52 -0400

    Update transformation.py

--- transformation.py
+++ transformation.py
@@ -6,5 +6,7 @@ import torchvision.transforms as transforms
 import sklearn.model_selection as model_selection
 
 ## transformations
+## transform = transforms.Compose([transforms.ToTensor(),transforms.CenterCrop((200,100)), transforms.Normalize((mean_and_std[0]),(mean_and_std[1])])
+                                
 transform = transforms.Compose([transforms.ToTensor(),transforms.CenterCrop((200,100)), 
-                                transforms.Normalize((mean_and_std[0]),(mean_and_std[1])])
+                                transforms.Normalize((0.4363,0.4328,0.3291),(0.2132,0.2078,0.2040))])
